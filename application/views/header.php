3547ae1d3eacd34b6568d6f6295030f806ec25e0
Sylvain Bouchet <sylvain.bouchet@globalis-ms.com>
2016-11-09 14:06:42 +0100

    Ajout CSS + JS + header + footer

---
+++ application/views/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <link rel="stylesheet" href="<?= base_url() . 'css/bootstrap.min.css' ?>" type="text/css">
+        <link rel="stylesheet" href="<?= base_url() . 'css/global.css' ?>" type="text/css">
+        <script src="<?= base_url() . 'js/jquery-3.1.1.min.js' ?>"></script>
+        <script src="<?= base_url() . 'js/bootstrap.min.js' ?>"></script>
+        <script src="<?= base_url() . 'js/hightcharts/highcharts.js' ?>"></script>
+        <script src="<?= base_url() . 'js/hightcharts/hightcharts_theme.js' ?>"></script>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="<?php echo site_url() ?>"> WoW_API </a>
+                </div>
+                <div class="collapse navbar-collapse">
+
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
